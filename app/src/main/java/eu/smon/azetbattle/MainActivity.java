4b9c2bed135aedac75ff2d418ea38d067fd4d3b7
mkiac <kiac@metait.cz>
2018-04-20 01:18:38 +0200

    komit 1

--- app/src/main/java/eu/smon/azetbattle/MainActivity.java
+++ app/src/main/java/eu/smon/azetbattle/MainActivity.java
@@ -2,8 +2,11 @@ package eu.smon.azetbattle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
