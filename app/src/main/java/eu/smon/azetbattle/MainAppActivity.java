8d0692f0f144a41adbf8b45eb534d5340b003cb2
mkiac <kiac@metait.cz>
2018-04-20 15:56:28 +0200

    nove ui

--- app/src/main/java/eu/smon/azetbattle/MainAppActivity.java
+++ app/src/main/java/eu/smon/azetbattle/MainAppActivity.java
@@ -196,22 +196,21 @@ public class MainAppActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+
         int id = item.getItemId();
 
-        /*if (id == R.id.nav_camera) {
+        if (id == R.id.nav_main) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }*/
+        }
+        else if (id == R.id.nav_edit_user) {
+            startActivity(new Intent(getApplicationContext(), EditUserActivity.class));
+        }
+        else if (id == R.id.nav_logout) {
+            // log out
+        }
+        else if (id == R.id.nav_about) {
+            startActivity(new Intent(getApplicationContext(), AboutAppActivity.class));
+        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
